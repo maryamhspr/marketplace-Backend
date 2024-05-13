8fbd9a822ce1e974a523d04c7f231ee7fd560a9f
Mary Hspr <maryam.hspr8@gmail.com>
2024-05-13 16:22:07 +0430

    add document for SupplierController

--- src/main/java/ir/irisa/marketplace/ws/controller/SupplierController.java
+++ src/main/java/ir/irisa/marketplace/ws/controller/SupplierController.java
@@ -1,5 +1,9 @@
 package ir.irisa.marketplace.ws.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import ir.irisa.marketplace.shared.model.BaseApiResponse;
 import ir.irisa.marketplace.ws.model.request.SupplierRequest;
@@ -19,11 +23,47 @@ public class SupplierController {
     }
 
     @PostMapping
+    @Operation(summary = "Add a new supplier into database")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "A supplier with this username is already exist",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Return a stored supplier",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Error in getting role",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "System Default Exception (SDE), or when database IO exception occurred",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            )
+    })
     public ResponseEntity<BaseApiResponse> createASupplier(@Valid @RequestBody SupplierRequest supplierRequest){
         return supplierService.createASupplier(supplierRequest);
     }
 
     @GetMapping
+    @Operation(summary = "Get list of suppliers")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Return list of suppliers",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "System Default Exception (SDE), or when database IO exception occurred",
+                    content = {@Content(mediaType = "application/json"), @Content(mediaType = "application/xml")}
+            )
+    })
     public ResponseEntity<BaseApiResponse> getAllSupplier(){
         return supplierService.getAllSupplier();
     }
--- src/main/java/ir/irisa/marketplace/ws/model/request/SupplierRequest.java
+++ src/main/java/ir/irisa/marketplace/ws/model/request/SupplierRequest.java
@@ -1,5 +1,6 @@
 package ir.irisa.marketplace.ws.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,11 +9,18 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class SupplierRequest {
+    @Schema(description = "Firstname for supplier", example = "Mahya")
     private String firstname;
+    @Schema(description = "Lastname for supplier", example = "Jafari")
     private String lastname;
+    @Schema(description = "Username for supplier", example = "M-Jafari")
     private String username;
+    @Schema(description = "Password for supplier", example = "123@bgh")
     private String password;
+    @Schema(description = "Role for supplier", example = "SUPPLIER")
     private String role;
+    @Schema(description = "Company name for supplier", example = "MhYA")
     private String companyName;
+    @Schema(description = "Enable for supplier", example = "true")
     private boolean enable;
 }
